61fc325e24b1a4b988161b2b94b8d80ec2f1e681
Void7 <limen191100@gmail.com>
2021-10-22 23:14:59 +0300

    Drawing frames
    Use function BeginDrawing and EndDrawing  to create a frame.
    Use function ClearBackground to  fill bg with color and avoid flickering.

--- axe_game.cpp
+++ axe_game.cpp
@@ -4,10 +4,12 @@ int main()
 {
     int width = 350, height = 200;
     InitWindow(width, height, "Void's window");
-    
+
         while (true)
     {
-
+        BeginDrawing();
+        ClearBackground(RED);
+        EndDrawing();
     }
 
     // false
