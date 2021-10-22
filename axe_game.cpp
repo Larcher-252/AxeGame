9072bb30787d03e8089ba02a65f45e8111cfba0f
Void7 <limen191100@gmail.com>
2021-10-22 21:47:48 +0300

    Work with float, double and bool
    Created float root_bear.
    Created double cheese_burger.
    Created bool shouldHaveLunch.

--- axe_game.cpp
+++ axe_game.cpp
@@ -4,4 +4,8 @@ int main()
 {
     int width = 350, height = 200;
     InitWindow(width, height, "Void's window");
+
+    float root_bear = 1.99f;
+    double cheese_burger = 5.99;
+    bool shouldHaveLunch = true;
 }
