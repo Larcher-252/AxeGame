8c363f074d266d02ab4f19cc26b856d05ce39307
Void7 <limen191100@gmail.com>
2021-10-23 00:14:49 +0300

    Add rectangle
    Add function DrawRectangle to draw rectangle on the canvas.
    Add some params for that rectangle.

--- axe_game.cpp
+++ axe_game.cpp
@@ -3,11 +3,17 @@
 int main()
 {
     // window dimensions
-    int width{350}, height{200};
+    int width{800}, height{450};
     InitWindow(width, height, "Void's window");
-    // circle coordinates
+    // circle params
     int circle_x{width / 2}, circle_y{height / 2};
-    int const circle_radius{25}, movementStep{5};
+    int const circle_radius{25};
+    // rectangle params
+    int rectangle_x{300}, rectangle_y{0};
+    int const rectangle_w{50}, rectangle_h{50};
+    // movement params
+    int const movementStep{5};
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
@@ -16,7 +22,7 @@ int main()
 
         // Game logic begins
         DrawCircle(circle_x, circle_y, circle_radius, PINK);
-
+        DrawRectangle(rectangle_x, rectangle_y, rectangle_w, rectangle_h, RED);
         if (IsKeyDown(KEY_D) && (circle_x < (width - circle_radius))) {circle_x += movementStep;}
         if (IsKeyDown(KEY_A) && (circle_x > circle_radius)) {circle_x -= movementStep;}
         if (IsKeyDown(KEY_S) && (circle_y < (height - circle_radius))) {circle_y += movementStep;}
