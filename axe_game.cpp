3167325e9f768c358359c4eab9ddfdabb4bb16ee
Void7 <limen191100@gmail.com>
2021-10-22 23:24:04 +0300

    Cross, X or ESC to close window
    Add function WindowShouldClose into while.

--- axe_game.cpp
+++ axe_game.cpp
@@ -5,7 +5,7 @@ int main()
     int width = 350, height = 200;
     InitWindow(width, height, "Void's window");
 
-        while (true)
+        while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RED);
