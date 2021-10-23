0b72d20bb93018249eb6648284b7facd57a8ee5b
Void7 <limen191100@gmail.com>
2021-10-23 14:32:36 +0300

    Logical OR
    Used logical operator or in if to make cube moving up and down.

--- axe_game.cpp
+++ axe_game.cpp
@@ -13,6 +13,7 @@ int main()
     int const rectangle_w{50}, rectangle_h{50};
     // movement params
     int const movementStep{5};
+    int cubeDirection{5};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -21,12 +22,16 @@ int main()
         ClearBackground(WHITE);
 
         // Game logic begins
+        // Circle
         DrawCircle(circle_x, circle_y, circle_radius, PINK);
-        DrawRectangle(rectangle_x, rectangle_y, rectangle_w, rectangle_h, RED);
         if (IsKeyDown(KEY_D) && (circle_x < (width - circle_radius))) {circle_x += movementStep;}
         if (IsKeyDown(KEY_A) && (circle_x > circle_radius)) {circle_x -= movementStep;}
         if (IsKeyDown(KEY_S) && (circle_y < (height - circle_radius))) {circle_y += movementStep;}
         if (IsKeyDown(KEY_W) && (circle_y > circle_radius)) {circle_y -= movementStep;}
+        // Rectangle
+        DrawRectangle(rectangle_x, rectangle_y, rectangle_w, rectangle_h, RED);
+        rectangle_y += cubeDirection;
+        if ((rectangle_y > (height - rectangle_h)) || (rectangle_y < 0)) {cubeDirection = -cubeDirection;}
         // Game logic ends
         EndDrawing();
     }
